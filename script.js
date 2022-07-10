a70b22206931c1004dae011067a4934b57c4a980
Elena Barinova <mkaumka@gmail.com>
2022-07-10 20:17:24 +0300

    lesson1

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const countProduct = 3;
 const categoryProduct ='vegetable';
 const priseProduct = 20;
 
-console.log('Name Product: ', nameProduct)
+console.log('Name Product: ', nameProduct);
 
-console.log('Grand total:' , countProduct*priseProduct )
+console.log('Grand total:' , countProduct*priseProduct);
 
