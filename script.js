6de288bfad11c59d932804fc5a6e27bb376a7f00
Elena Barinova <mkaumka@gmail.com>
2022-07-10 19:53:32 +0300

    init

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const nameProduct = 'tomato';
+const countProduct = 3;
+const categoryProduct ='vegetable';
+const priseProduct = 20;
+
+console.log('Name Product: ', nameProduct)
+
+console.log('Grand total:' , countProduct*priseProduct )
+
